655d8bf9989f9f72c6eecca51296b1db8d110f6c
Doug <me@douglasbowen.com>
2019-05-31 13:21:56 -0400

    add crud endpoints for actions

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const server = express();
 // routes for project and actions
-//const actionRouter = require("./data/helpers/actionRouter.js");
+const actionRouter = require("./data/helpers/actionRouter.js");
 const projectRouter = require("./data/helpers/projectRouter.js");
 
 server.use(express.json());
-//server.use("/actions", actionRouter);
+server.use("/actions", actionRouter);
 server.use("/projects", projectRouter);
 
 module.exports = server;
